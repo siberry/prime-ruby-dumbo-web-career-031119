24c3fd3b801e30b6ea28f886b922bec9761169ca
siberry <siberry226@gmail.com>
2019-02-21 20:37:53 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -7,9 +7,3 @@ def prime?(num)
     t_or_f = a_factor.include?(true) || num.abs == 1 || num == 0
     !t_or_f
 end
-
-
-#e.g. prime?(9) => false, range [2, 3, 4, 5, 6, 7, 8]
-# false, true, false, false, false, false, false
-#e.g. prime?(7) => true, range [2, 3, 4, 5, 6]
-# false, false, etc
