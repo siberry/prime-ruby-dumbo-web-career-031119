6dd9a38c69715342f084c8cf42188bb6d4f52251
siberry <siberry226@gmail.com>
2019-02-21 20:32:12 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(num)
-    poss_factors = (2...num.abs).to_a
+    poss_factors = (2...num.abs)
     a_factor = []
     poss_factors.each do |factor|
       a_factor.push(num % factor == 0)
